de676ef6ff9f37d70a67fd789d4713449cb04e1a
Juan Carlos Aguilar Rincón <Juan_Aguilar@epam.com>
2021-06-24 00:46:08 -0500

    complete demo 2

---
+++ 2_terraform_demo/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  tenant_id       = var.azure_tenant_id
+  subscription_id = var.azure_subscription_id
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = "eastus"
+
+  tags = {
+    Environment = "Terraform"
+    Team = "DevSecOps"
+  }
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "eastus"
+  resource_group_name = azurerm_resource_group.rg.name
+}
