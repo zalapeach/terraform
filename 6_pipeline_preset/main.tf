7bccf07bc4b4163c949611a7d410f7d81b47e541
Juan Carlos Aguilar Rincón <Juan_Aguilar@epam.com>
2021-07-05 13:46:25 -0500

    Added pipeline preset

---
+++ 6_pipeline_preset/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  tenant_id       = var.azure_tenant_id
+  subscription_id = var.azure_subscription_id
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "vaultRG"
+  location = "eastus"
+}
+
+resource "azurerm_key_vault" "keyvault" {
+  name                = "vaultRG"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg
+  sku_name            = "standard"
+}
