80ed89e8b9f24f5135d101f7e2791566679b99a3
Juan Carlos Aguilar Rincón <Juan_Aguilar@epam.com>
2021-07-07 14:04:19 -0500

    plan should works now

--- 7_pipeline_arch/main.tf
+++ 7_pipeline_arch/main.tf
@@ -5,22 +5,10 @@ terraform {
       version = "=2.46.0"
     }
   }
-
-  #backend "azurerm" {
-    #storage_account_name = "<change this>"
-    #container_name       = "<change this>"
-    #access_key           = "<change this>"
-    #key                  = "<change this>"
-  #}
 }
 
 provider "azurerm" {
   features {}
-
-  #client_id       = var.azure_client_id
-  #client_secret   = var.azure_client_secret
-  #tenant_id       = var.azure_tenant_id
-  #subscription_id = var.azure_subscription_id
 }
 
 resource "azurerm_resource_group" "rg" {
