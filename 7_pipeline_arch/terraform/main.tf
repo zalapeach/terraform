8798a6ff69a220a938d9d9c0b3aad6b9fd0fc327
Juan Carlos Aguilar Rincón <Juan_Aguilar@epam.com>
2021-07-08 21:47:38 -0500

    change http protocol to utp protocol

--- 7_pipeline_arch/terraform/main.tf
+++ 7_pipeline_arch/terraform/main.tf
@@ -59,7 +59,7 @@ resource "azurerm_network_security_group" "nsg" {
     priority                   = 1002
     direction                  = "Inbound"
     access                     = "Allow"
-    protocol                   = "HTTP"
+    protocol                   = "TCP"
     source_port_range          = "*"
     destination_port_range     = "80"
     source_address_prefix      = "*"
