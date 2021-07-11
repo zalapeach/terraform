bb369a724e995143066e94d751b955fdf0d4b540
Juan Carlos Aguilar Rincón <Juan_Aguilar@epam.com>
2021-07-11 12:31:37 -0500

    Fix subnet name

--- 7_pipeline_arch/terraform/main.tf
+++ 7_pipeline_arch/terraform/main.tf
@@ -35,14 +35,14 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_subnet" "SubnetFront" {
+resource "azurerm_subnet" "subnetFront" {
   name                 = "frontend"
   address_prefixes     = ["10.0.1.0/24"]
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
 }
 
-resource "azurerm_subnet" "SubnetBack" {
+resource "azurerm_subnet" "subnetBack" {
   name                 = "frontend"
   address_prefixes     = ["10.0.2.0/24"]
   resource_group_name  = azurerm_resource_group.rg.name
