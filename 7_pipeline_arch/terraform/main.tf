05d1b70275c165b8bd2fed44b3fd972edca6f142
Juan Carlos Aguilar Rincón <Juan_Aguilar@epam.com>
2021-07-11 12:21:24 -0500

    fix issues with terraform files

--- 7_pipeline_arch/terraform/main.tf
+++ 7_pipeline_arch/terraform/main.tf
@@ -19,7 +19,7 @@ provider "azurerm" {
 
   client_id       = var.azure_client_id
   client_secret   = var.azure_client_secret
-  tenand_id       = var.azure_tenant_id
+  tenant_id       = var.azure_tenant_id
   subscription_id = var.azure_subscription_id
 }
 
