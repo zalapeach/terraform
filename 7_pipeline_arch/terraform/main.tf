971af5c34808b5b95368552dcadd8234e9be619e
Juan Carlos Aguilar Rincón <Juan_Aguilar@epam.com>
2021-07-11 13:43:30 -0500

    Added vms and bastion

--- 7_pipeline_arch/terraform/main.tf
+++ 7_pipeline_arch/terraform/main.tf
@@ -43,11 +43,13 @@ resource "azurerm_subnet" "subnetFront" {
 }
 
 resource "azurerm_subnet" "subnetBack" {
-  name                 = "frontend"
+  name                 = "backend"
   address_prefixes     = ["10.0.2.0/24"]
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
 }
+
+# Creates all stuff for Application gateway
 
 resource "azurerm_public_ip" "ip" {
   name                = "myDinamicIP"
@@ -108,5 +110,112 @@ resource "azurerm_application_gateway" "appGateway" {
     http_listener_name         = "vnet-listener"
     backend_address_pool_name  = "vnet-backpool"
     backend_http_settings_name = "vnet-httpSet"
+  }
+}
+
+# start prerequisites for frontend VMs
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "netSecGrp"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "httpSecRule"
+    priority                   = 1001
+    direction                  = "inbound"
+    access                     = "allow"
+    protocol                   = "TCP"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface" "nic" {
+  count               = 2
+  name                = "nic0${count.index}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name = "nicIPConf"
+    subnet_id = azurerm_subnet.subnetFront.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_availability_set" "as" {
+  name                         = "availabilitySet"
+  location                     = azurerm_resource_group.rg.location
+  resource_group_name          = azurerm_resource_group.rg.name
+  platform_fault_domain_count  = 2
+  platform_update_domain_count = 2
+  managed                      = true
+}
+
+resource "azurerm_network_interface_security_group_association" "nsga" {
+  count                     = 2
+  network_interface_id      = element(azurerm_network_interface.nic.*.id, count.index)
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  count                 = 2
+  name                  = "vm0${count.index}"
+  location              = azurerm_resource_group.rg.location
+  availability_set_id   = azurerm_availability_set.as.id
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_inferface_ids = [element(azurerm_network_interface.nic.*.id, count.index)]
+  size                  = "Standard_DS1_v2"
+
+  os_disk {
+    name                 = "osDisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-LTS"
+    version   = "latest"
+  }
+
+  computer_name  = "vm0${count.index}"
+  admin_username = "zala"
+  admin_password = "tamarindo"
+
+  tags = {
+    env = "vm"
+  }
+}
+
+# Creating Bastion
+
+resource "azurerm_subnet" "subnetBastion" {
+  name                 = "bastion"
+  address_prefixes     = ["10.0.3.0/24"]
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+}
+
+resource "azurerm_public_ip" "bastionIP" {
+  name                = "bastionIP"
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Dynamic"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_bastion_host" "bastion" {
+  name                = "myBastion"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                 = "IPConf"
+    subnet_id            = azurerm_subnet.bastion.id
+    public_ip_address_id = azurerm_public_ip.bastionIP.id
   }
 }
