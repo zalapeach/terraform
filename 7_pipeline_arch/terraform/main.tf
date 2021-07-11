80b1765c330b59dae8c2bb1b985921f16dfdc48d
Juan Carlos Aguilar Rincón <Juan_Aguilar@epam.com>
2021-07-11 14:19:09 -0500

    change name for bastion subnet

--- 7_pipeline_arch/terraform/main.tf
+++ 7_pipeline_arch/terraform/main.tf
@@ -195,7 +195,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 # Creating Bastion
 
 resource "azurerm_subnet" "subnetBastion" {
-  name                 = "bastion"
+  name                 = "AzureBastionSubnet"
   address_prefixes     = ["10.0.3.0/24"]
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
