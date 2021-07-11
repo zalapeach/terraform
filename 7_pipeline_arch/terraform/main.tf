5ff4b8e9abb2490f72376f030ab364babea1b774
Juan Carlos Aguilar Rincón <Juan_Aguilar@epam.com>
2021-07-11 12:18:35 -0500

    create application gateway with terraform

--- 7_pipeline_arch/terraform/main.tf
+++ 7_pipeline_arch/terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     azurerm = {
-      source =  "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "=2.46.0"
     }
   }
@@ -19,125 +19,94 @@ provider "azurerm" {
 
   client_id       = var.azure_client_id
   client_secret   = var.azure_client_secret
-  tenant_id       = var.azure_tenant_id
+  tenand_id       = var.azure_tenant_id
   subscription_id = var.azure_subscription_id
 }
 
 resource "azurerm_resource_group" "rg" {
-  name = "azdevopsRG"
+  name     = "azdevopsRG"
   location = "eastus"
 }
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "myVnet01"
-  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16"]
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_subnet" "subnet" {
-  name                 = "mySubnet"
+resource "azurerm_subnet" "SubnetFront" {
+  name                 = "frontend"
+  address_prefixes     = ["10.0.1.0/24"]
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "SubnetBack" {
+  name                 = "frontend"
+  address_prefixes     = ["10.0.2.0/24"]
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
 }
 
 resource "azurerm_public_ip" "ip" {
   name                = "myDinamicIP"
   location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
+  resource_group_name = azurerm_resource_group.name
 }
 
-resource "azurerm_network_security_group" "nsg" {
-  name                = "myNSG"
+resource "azurerm_application_gateway" "appGateway" {
+  name                = "myAppGateway"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
-  security_rule {
-    name                       = "http"
-    priority                   = 1002
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "TCP"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
+  sku {
+    name     = "Standard_Small"
+    tier     = "Standard"
+    capacity = 2
   }
 
-  security_rule {
-    name                       = "ssh"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "TCP"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-resource "azurerm_network_interface" "nic" {
-  name                = "myNIC"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "confNIC"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.ip.id
-
-  }
-}
-
-resource "azurerm_network_interface_security_group_association" "nsga" {
-  network_interface_id      = azurerm_network_interface.nic.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
-}
-
-resource "tls_private_key" "ssh_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-output "tls_private_key" {
-  value     = tls_private_key.ssh_key.private_key_pem
-  sensitive = true
-}
-
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                  = "mysql01"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.nic.id]
-  size                  = "Standard_DS1_v2"
-
-  os_disk {
-    name    = "osDisk"
-    caching = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+  gateway_ip_configuration {
+    name      = "myGatewayIPConf"
+    subnet_id = azurerm_subnet.subnetFront.id
   }
 
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-focal"
-    sku       = "20_04-LTS"
-    version   = "latest"
+  frontend_port {
+    name = "vnet-frontendPort"
+    port = 80
   }
 
-  computer_name                   = "mysql01"
-  admin_username                  = "zala"
-  disable_password_authentication = true
-
-  admin_ssh_key {
-    username = "zala"
-    public_key = file("./id_rsa.pub")
+  frontend_ip_configuration {
+    name                 = "vnet-frontendIP"
+    public_ip_address_id = azurerm_public_ip.ip.id
   }
 
-  tags = {
-    env = "db"
+  backend_address_pool {
+    name = "vnet-backpool"
+  }
+
+  backend_http_settings {
+    name                  = "vnet-httpSet"
+    path                  = "/"
+    port                  = 80
+    protocol              = "http"
+    request_timeout       = 60
+    cookie_based_affinity = "disabled"
+  }
+
+  http_listener {
+    name                           = "vnet-listener"
+    protocol                       = "http"
+    frontend_port_name             = "vnet-frontendPort"
+    frontend_ip_configuration_name = "vnet-frontendIP"
+  }
+
+  request_routing_rule {
+    name                       = "vnet-routingRule"
+    rule_type                  = "Basic"
+    http_listener_name         = "vnet-listener"
+    backend_address_pool_name  = "vnet-backpool"
+    backend_http_settings_name = "vnet-httpSet"
   }
 }
