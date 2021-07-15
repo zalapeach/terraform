d312c0c7011a33513d107ff749e2d3b3d6b4981c
Juan Carlos Aguilar Rincón <Juan_Aguilar@epam.com>
2021-07-15 18:35:54 -0500

    add password

--- 7_pipeline_arch/terraform/main.tf
+++ 7_pipeline_arch/terraform/main.tf
@@ -251,6 +251,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   computer_name  = "${local.names[count.index].name}0${count.index}"
   admin_username = "zala"
+  admin_password = "123@T4M4R1N-do"
 
   admin_ssh_key {
     username   = "zala"
