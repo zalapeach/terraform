281393ce58bf06f0e2ecc64e8b9288e947f06591
Juan Carlos Aguilar Rincón <Juan_Aguilar@epam.com>
2021-07-19 09:51:47 -0500

    Add missing playbooks

---
+++ 7_pipeline_arch/ansible/test.sh
@@ -0,0 +1,7 @@
+curl "http://40.80.145.18/wordpress/wp-admin/install.php?step=2" \
+  --data-urlencode "weblog_title=Nintendomania" \
+  --data-urlencode "user_name=zala" \
+  --data-urlencode "admin_email=zalapeach@gmail.com" \
+  --data-urlencode "admin_password=tamarindo" \
+  --data-urlencode "admin_password2=tamarindo" \
+  --data-urlencode "pw_weak=1"
