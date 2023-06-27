be3a9e470e73a3173863d5dddadf612d002535c7
zalapeach <juan.aguilar@thomsonreuters.com>
2023-06-27 15:57:50 -0500

    creates a variable set with one variable

--- azure_devops/tfCloud.tf
+++ azure_devops/tfCloud.tf
@@ -7,3 +7,23 @@ resource "tfe_workspace" "workspace" {
   name         = "azdo"
   organization = tfe_organization.org.name
 }
+
+resource "tfe_variable_set" "varset" {
+  name         = "Terraform credentials"
+  description  = "Terraform credentials used for build infrastructure"
+  organization = tfe_organization.org.name
+}
+
+resource "tfe_workspace_variable_set" "workvarset" {
+  variable_set_id = tfe_variable_set.varset.id
+  workspace_id    = tfe_workspace.workspace.id
+}
+
+resource "tfe_variable" "tfetoken" {
+  key             = "TFE_TOKEN"
+  value           = var.tfe_token
+  category        = "env"
+  description     = "Terraform Cloud Token"
+  variable_set_id = tfe_variable_set.varset.id
+  sensitive       = true
+}
