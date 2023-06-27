c155dde3438011f4c44d8d17701fc86fc31d94a9
zalapeach <juan.aguilar@thomsonreuters.com>
2023-06-27 16:49:56 -0500

    fix errors with terraform cloud

--- azure_devops/tfCloud.tf
+++ azure_devops/tfCloud.tf
@@ -1,21 +1,23 @@
 resource "tfe_organization" "org" {
-  name  = "zalapeach"
-  email = var.org_email
+  name                          = "zalapeach"
+  email                         = var.org_email
+  allow_force_delete_workspaces = true
 }
 
 resource "tfe_workspace" "workspace" {
   name         = "azdo"
   organization = tfe_organization.org.name
+  force_delete = true
 }
 
-resource "tfe_variable_set" "varset" {
+resource "tfe_variable_set" "vars" {
   name         = "Terraform credentials"
   description  = "Terraform credentials used for build infrastructure"
   organization = tfe_organization.org.name
 }
 
 resource "tfe_workspace_variable_set" "workvarset" {
-  variable_set_id = tfe_variable_set.varset.id
+  variable_set_id = tfe_variable_set.vars.id
   workspace_id    = tfe_workspace.workspace.id
 }
 
@@ -24,6 +26,6 @@ resource "tfe_variable" "tfetoken" {
   value           = var.tfe_token
   category        = "env"
   description     = "Terraform Cloud Token"
-  variable_set_id = tfe_variable_set.varset.id
+  variable_set_id = tfe_variable_set.vars.id
   sensitive       = true
 }
