5d93bd74cc47cf6de4fa77bada7f7a00025c42da
zalapeach <juan.aguilar@thomsonreuters.com>
2023-07-05 16:25:48 -0500

    fix source_image_reference

--- azure/004_linux_vm/main.tf
+++ azure/004_linux_vm/main.tf
@@ -99,8 +99,8 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "22.04-LTS"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
     version   = "latest"
   }
 
