6435f9d082f7fda34bca822ee80a297d19e028a9
zalapeach <juan.aguilar@thomsonreuters.com>
2023-07-05 16:10:57 -0500

    adds nic

--- azure/004_linux_vm/main.tf
+++ azure/004_linux_vm/main.tf
@@ -85,10 +85,11 @@ resource "tls_private_key" "sshkey" {
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "linux_vm"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  size                = "Standard_DS1_v2"
+  name                  = "linux_vm"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  size                  = "Standard_DS1_v2"
 
   os_disk {
     name                 = "os_disk"
