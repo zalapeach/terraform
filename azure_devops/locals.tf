133a7726cc5db234184d0f24bbbf816abe324714
zalapeach <juan.aguilar@thomsonreuters.com>
2023-07-10 13:52:44 -0500

    preparing example lb with terraform

--- azure_devops/locals.tf
+++ azure_devops/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  workspaces = ["azdo", "001", "002", "003", "004"]
+  workspaces = ["azdo", "001", "002", "003", "004", "005"]
   variables = {
     ARM_CLIENT_ID = {
       value       = azuread_application.app.application_id
