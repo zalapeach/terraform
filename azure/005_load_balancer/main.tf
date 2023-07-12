76c4c96ff9061d47e17f574bcc2e30079bb7e7fa
zalapeach <juan.aguilar@thomsonreuters.com>
2023-07-12 14:56:17 -0500

    adds nic, availabilityset and backendpool association with nics

--- azure/005_load_balancer/main.tf
+++ azure/005_load_balancer/main.tf
@@ -93,3 +93,31 @@ resource "azurerm_lb_rule" "lb_rule" {
   backend_address_pool_ids       = [azurerm_lb_backend_address_pool.lb_bp.id]
   probe_id                       = azurerm_lb_probe.lb_hp.id
 }
+
+resource "azurerm_network_interface" "nic" {
+  count               = 2
+  name                = "nic0${count.index}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "nic_ip_conf"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_network_interface_backend_address_pool_association" "nic_bp" {
+  count                   = 2
+  network_interface_id    = element(azurerm_network_interface.nic.*.id, count.index)
+  ip_configuration_name   = "nic_ip_conf"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.lb_bp.id
+}
+
+resource "azurerm_availability_set" "as" {
+  name                         = "availabilitySet"
+  location                     = azurerm_resource_group.rg.location
+  resource_group_name          = azurerm_resource_group.rg.name
+  platform_fault_domain_count  = 2
+  platform_update_domain_count = 2
+}
