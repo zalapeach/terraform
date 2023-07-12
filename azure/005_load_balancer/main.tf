7ba415477af883fc1dbb65a96dfd5dbb2f307118
zalapeach <juan.aguilar@thomsonreuters.com>
2023-07-12 15:08:10 -0500

    adds outputs, also a ssh key and a nic - nsg association

--- azure/005_load_balancer/main.tf
+++ azure/005_load_balancer/main.tf
@@ -96,7 +96,7 @@ resource "azurerm_lb_rule" "lb_rule" {
 
 resource "azurerm_network_interface" "nic" {
   count               = 2
-  name                = "nic0${count.index}"
+  name                = "nic0${ count.index }"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -120,4 +120,15 @@ resource "azurerm_availability_set" "as" {
   resource_group_name          = azurerm_resource_group.rg.name
   platform_fault_domain_count  = 2
   platform_update_domain_count = 2
+}
+
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "azurerm_network_interface_security_group_association" "nsga" {
+  count                     = 2
+  network_interface_id      = element(azurerm_network_interface.nic.*.id, count.index)
+  network_security_group_id = azurerm_network_security_group.nsg.id
 }
