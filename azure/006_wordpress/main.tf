939489b141d981ea16ae61fb7246c887f6c858a4
zalapeach <juan.aguilar@thomsonreuters.com>
2023-07-17 14:30:03 -0500

    Added gateway

--- azure/006_wordpress/main.tf
+++ azure/006_wordpress/main.tf
@@ -42,3 +42,58 @@ resource "azurerm_public_ip" "pip" {
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
 }
+
+resource "azurerm_application_gateway" "gw" {
+  name                = "AppGW"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  sku {
+    name     = "Standard_Small"
+    tier     = "Standard"
+    capacity = 2
+  }
+
+  gateway_ip_configuration {
+    name      = "GWIpConf"
+    subnet_id = azurerm_subnet.frontSubnet.id
+  }
+
+  frontend_port {
+    name = "frontendPort"
+    port = 80
+  }
+
+  frontend_ip_configuration {
+    name                 = "frontendIp"
+    public_ip_address_id = azurerm_public_ip.pip.id
+  }
+
+  backend_address_pool {
+    name = "backendPool"
+  }
+
+  backend_http_settings {
+    name                  = "httpSettings"
+    path                  = "/"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 60
+    cookie_based_affinity = "Disabled"
+  }
+
+  http_listener {
+    name                           = "listener"
+    protocol                       = "Http"
+    frontend_port_name             = "frontendPort"
+    frontend_ip_configuration_name = "frontendIp"
+  }
+
+  request_routing_rule {
+    name                       = "routingRule"
+    rule_type                  = "Basic"
+    http_listener_name         = "listener"
+    backend_address_pool_name  = "backendPool"
+    backend_http_settings_name = "httpSettings"
+  }
+}
