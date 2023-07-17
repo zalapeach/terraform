e45627e6bd42f06aebf3557b148b8be8efdfbc50
zalapeach <juan.aguilar@thomsonreuters.com>
2023-07-17 14:02:01 -0500

    created workspace 006

---
+++ azure/006_wordpress/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.61.0"
+    }
+  }
+  cloud {
+    organization = "zalapeach"
+    workspaces {
+      name = "006"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "resourceGroup006"
+  location = "westus2"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet01"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "frontSubnet" {
+  name                 = "frontend"
+  address_prefixes     = ["10.0.0.0/24"]
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "WordPressIP"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
--- azure_devops/locals.tf
+++ azure_devops/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  workspaces = ["azdo", "001", "002", "003", "004", "005"]
+  workspaces = ["azdo", "001", "002", "003", "004", "005", "006"]
   variables = {
     ARM_CLIENT_ID = {
       value       = azuread_application.app.application_id
