5a49e8f67eaf89b3a85f5f8223c69f7cbe1d5e38
zalapeach <juan.aguilar@thomsonreuters.com>
2023-07-19 16:45:14 -0500

    Change allocation method

--- azure/006_wordpress/main.tf
+++ azure/006_wordpress/main.tf
@@ -40,7 +40,7 @@ resource "azurerm_public_ip" "pip" {
   name                = "WordPressIP"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
   sku                 = "Standard"
 }
 
