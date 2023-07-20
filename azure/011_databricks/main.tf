e7ae488db9196a2728b35bfd50bd2759bc54b34d
zalapeach <juan.aguilar@thomsonreuters.com>
2023-07-20 18:17:16 -0500

    adds databricks tf code

--- azure/011_databricks/main.tf
+++ azure/011_databricks/main.tf
@@ -11,3 +11,23 @@ resource "azurerm_databricks_workspace" "databricks" {
   location            = azurerm_resource_group.rg.location
   sku                 = "premium"
 }
+
+data "databricks_node_type" "smallest" {
+  local_disk = true
+
+  depends_on = [azurerm_databricks_workspace.databricks]
+}
+
+data "databricks_spark_version" "latest_lts" {
+  long_term_support = true
+
+  depends_on = [azurerm_databricks_workspace.databricks]
+}
+
+resource "databricks_cluster" "cluster" {
+  cluster_name            = "cluster"
+  node_type_id            = data.databricks_node_type.smallest.id
+  spark_version           = data.databricks_spark_version.latest_lts.id
+  autotermination_minutes = 60
+  num_workers             = 1
+}
