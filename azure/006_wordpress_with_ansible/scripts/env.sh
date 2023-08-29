ed0e74b2524b2a040dd174c24c76a066f94c7051
zalapeach <juan.aguilar@thomsonreuters.com>
2023-08-29 13:31:13 -0500

    fix env.sh script

--- azure/006_wordpress_with_ansible/scripts/env.sh
+++ azure/006_wordpress_with_ansible/scripts/env.sh
@@ -2,10 +2,10 @@
 
 cat <<EOF
 {
-  "azure_devops_pat": $AZDO_PERSONAL_ACCESS_TOKEN,
-  "azure_devops_org": $AZDO_ORG_SERVICE_URL,
-  "azure_app_id": $ARM_CLIENT_ID,
-  "azure_app_secret": $ARM_CLIENT_SECRET,
-  "azure_app_tenant": $ARM_TENANT_ID
+  "azure_devops_pat": "$AZDO_PERSONAL_ACCESS_TOKEN",
+  "azure_devops_org": "$AZDO_ORG_SERVICE_URL",
+  "azure_app_id": "$ARM_CLIENT_ID",
+  "azure_app_secret": "$ARM_CLIENT_SECRET",
+  "azure_app_tenant": "$ARM_TENANT_ID"
 }
 EOF
