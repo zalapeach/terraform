1ac050adfae01d62907cd97ad125134c6f8da738
zalapeach <juan.aguilar@thomsonreuters.com>
2023-08-30 15:37:00 -0500

    update script

--- azure/006_wordpress_with_ansible/scripts/agent.sh
+++ azure/006_wordpress_with_ansible/scripts/agent.sh
@@ -8,10 +8,12 @@ sudo apt upgrade
 sudo apt install ansible -y
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 sudo az login --service-principal -u $APPID -p $SECRET --tenant $TENANT
+sudo mkdir /myagent
+cd /myagent
 sudo wget https://vstsagentpackage.azureedge.net/agent/3.225.0/vsts-agent-linux-x64-3.225.0.tar.gz
 sudo tar zxvf ./vsts-agent-linux-x64-2.188.4.tar.gz
 sudo chmod -R 777 /myagent
-runuser -l zala -c "/myagent/config.sh --unattended --url https://dev.azure.com/${ORG} --auth pat --token ${PAT} --pool SelfHosted"
+runuser -l zala -c "/myagent/config.sh --unattended --url ${ORG} --auth pat --token ${PAT} --pool SelfHosted"
 sudo /myagent/svc.sh install
 sudo /myagent/svc.sh start
 exit 0
