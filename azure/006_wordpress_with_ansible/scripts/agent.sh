d4cf9aa737cc753e006d2c608e7c6961d638ae16
zalapeach <juan.aguilar@thomsonreuters.com>
2023-08-31 13:57:53 -0500

    update script for agent installation

--- azure/006_wordpress_with_ansible/scripts/agent.sh
+++ azure/006_wordpress_with_ansible/scripts/agent.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+
 PAT=$1
 ORG=$2
 APPID=$3
 SECRET=$4
 TENANT=$5
+whoami
 sudo apt update
 sudo apt upgrade
 sudo apt install ansible -y
@@ -12,8 +15,7 @@ sudo mkdir /home/zala/myagent
 cd /home/zala/myagent
 wget https://vstsagentpackage.azureedge.net/agent/3.225.0/vsts-agent-linux-x64-3.225.0.tar.gz
 tar zxvf ./vsts-agent-linux-x64-3.225.0.tar.gz
-chmod -R 777 myagent/
-runuser -l zala -c "/myagent/config.sh --unattended --url ${ORG} --auth pat --token ${PAT} --pool SelfHosted"
-/myagent/svc.sh install
-/myagent/svc.sh start
+runuser -l zala -c "./config.sh --unattended --url ${ORG} --auth pat --token ${PAT} --pool SelfHosted"
+./svc.sh install
+./svc.sh start
 exit 0
