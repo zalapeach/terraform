0b2d0c79202889077aecc2cb7fd3d235d6a8463c
zalapeach <juan.aguilar@thomsonreuters.com>
2023-09-05 11:30:48 -0500

    fix tar path

--- azure/006_wordpress_with_ansible/scripts/agent.sh
+++ azure/006_wordpress_with_ansible/scripts/agent.sh
@@ -13,7 +13,7 @@ az login --service-principal -u $APPID -p $SECRET --tenant $TENANT
 mkdir /myagent
 cd /myagent
 wget https://vstsagentpackage.azureedge.net/agent/3.225.0/vsts-agent-linux-x64-3.225.0.tar.gz
-tar zxvf /agent/vsts-agent-linux-x64-3.225.0.tar.gz
+tar zxvf /myagent/vsts-agent-linux-x64-3.225.0.tar.gz
 chmod -R 777 /myagent
 runuser -l zala -c "/myagent/config.sh --unattended --url ${ORG} --auth pat --token ${PAT} --pool SelfHosted"
 /myagent/svc.sh install
