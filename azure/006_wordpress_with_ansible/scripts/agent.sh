d8b166f26895bdd64c94a9a7897b19035dfdd800
zalapeach <juan.aguilar@thomsonreuters.com>
2023-09-20 15:14:30 -0500

    comment script

--- azure/006_wordpress_with_ansible/scripts/agent.sh
+++ azure/006_wordpress_with_ansible/scripts/agent.sh
@@ -5,21 +5,21 @@ ORG=$2
 APPID=$3
 SECRET=$4
 TENANT=$5
-apt update
-apt upgrade
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
-az login --service-principal -u $APPID -p $SECRET --tenant $TENANT
-apt install plocate
-apt install python3 -y
-apt install python3-pip -y
-python3 -m pip install ansible
-python3 -m pip install -r https://raw.githubusercontent.com/ansible-collections/azure/dev/requirements-azure.txt
-mkdir /myagent
-cd /myagent
-wget https://vstsagentpackage.azureedge.net/agent/3.225.0/vsts-agent-linux-x64-3.225.0.tar.gz
-tar zxvf /myagent/vsts-agent-linux-x64-3.225.0.tar.gz
-chmod -R 777 /myagent
-runuser -l zala -c "/myagent/config.sh --unattended --url ${ORG} --auth pat --token ${PAT} --pool SelfHosted"
-/myagent/svc.sh install
-/myagent/svc.sh start
+#runuser -l zala -c "sudo apt update"
+#runuser -l zala -c "sudo apt upgrade"
+#runuser -l zala -c "curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash"
+#runuser -l zala -c "az login --service-principal -u $APPID -p $SECRET --tenant $TENANT"
+#runuser -l zala -c "sudo apt install plocate"
+#runuser -l zala -c "sudo apt install python3 -y"
+#runuser -l zala -c "sudo apt install python3-pip -y"
+#runuser -l zala -c "python3 -m pip install ansible"
+#runuser -l zala -c "python3 -m pip install -r https://raw.githubusercontent.com/ansible-collections/azure/dev/requirements-azure.txt"
+#runuser -l zala -c "mkdir /myagent"
+#cd /myagent
+#runuser -l zala -c "wget https://vstsagentpackage.azureedge.net/agent/3.225.0/vsts-agent-linux-x64-3.225.0.tar.gz"
+#runuser -l zala -c "tar zxvf /myagent/vsts-agent-linux-x64-3.225.0.tar.gz"
+#chmod -R 777 /myagent
+#runuser -l zala -c "/myagent/config.sh --unattended --url ${ORG} --auth pat --token ${PAT} --pool SelfHosted"
+#/myagent/svc.sh install
+#/myagent/svc.sh start
 exit 0
