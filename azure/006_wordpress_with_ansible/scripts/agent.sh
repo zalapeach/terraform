622e8151ec0b1c56c3d0896bd6da309cc15069cc
zalapeach <juan.aguilar@thomsonreuters.com>
2023-09-21 15:03:18 -0500

    fix script

--- azure/006_wordpress_with_ansible/scripts/agent.sh
+++ azure/006_wordpress_with_ansible/scripts/agent.sh
@@ -5,8 +5,8 @@ ORG=$2
 APPID=$3
 SECRET=$4
 TENANT=$5
-#runuser -l zala -c "sudo apt update"
-#runuser -l zala -c "sudo apt upgrade"
+#runuser -l zala -c "sudo apt update -y"
+#runuser -l zala -c "sudo apt upgrade -y"
 #runuser -l zala -c "curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash"
 #runuser -l zala -c "az login --service-principal -u $APPID -p $SECRET --tenant $TENANT"
 #runuser -l zala -c "sudo apt install plocate -y"
@@ -15,9 +15,11 @@ TENANT=$5
 #runuser -l zala -c "python3 -m pip install ansible"
 #runuser -l zala -c "python3 -m pip install -r https://raw.githubusercontent.com/ansible-collections/azure/dev/requirements-azure.txt"
 #runuser -l zala -c "mkdir /home/zala/myagent"
-#runuser -l zala -c "wget https://vstsagentpackage.azureedge.net/agent/3.225.0/vsts-agent-linux-x64-3.225.0.tar.gz"
-#runuser -l zala -c "tar zxvf /home/zala/myagent/vsts-agent-linux-x64-3.225.0.tar.gz"
-#runuser -l zala -c "/home/zala/myagent/config.sh --unattended --url ${ORG} --auth pat --token ${PAT} --pool SelfHosted"
-#runuser -l zala -c "sudo /myagent/svc.sh install"
-#runuser -l zala -c "/myagent/svc.sh start"
+#runuser -l zala -c "cd /home/zala/myagent"
+#runuser -l zala -c "wget -P https://vstsagentpackage.azureedge.net/agent/3.225.0/vsts-agent-linux-x64-3.225.0.tar.gz"
+#runuser -l zala -c "tar zxvf ./vsts-agent-linux-x64-3.225.0.tar.gz"
+#runuser -l zala -c "./config.sh --unattended --url ${ORG} --auth pat --token ${PAT} --pool SelfHosted"
+#runuser -l zala -c "echo /home/zala/.local/bin:$PATH > ./.path"
+#runuser -l zala -c "sudo ./myagent/svc.sh install"
+#runuser -l zala -c "sudo ./myagent/svc.sh start"
 exit 0
