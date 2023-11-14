6d9ed859b8190131d7885ccbe7baeb9cd442a9a4
Juan Aguilar <Juan.Aguilar@epam.com>
2023-11-14 15:57:06 -0600

    update output and playbook

--- azure/006_wordpress_with_ansible/outputs.tf
+++ azure/006_wordpress_with_ansible/outputs.tf
@@ -11,3 +11,12 @@ output "tls_private_key" {
 output "appgtw_public_ip" {
   value = azurerm_public_ip.pip.ip_address
 }
+
+output "private_ips" {
+  value = {
+    "${azurerm_linux_virtual_machine.vms[0].name}": azurerm_linux_virtual_machine.vms[0].private_ips_address,
+    "${azurerm_linux_virtual_machine.vms[1].name}": azurerm_linux_virtual_machine.vms[1].private_ips_address,
+    "${azurerm_linux_virtual_machine.vms[2].name}": azurerm_linux_virtual_machine.vms[2].private_ips_address,
+    "${azurerm_linux_virtual_machine.vms[3].name}": azurerm_linux_virtual_machine.vms[3].private_ips_address
+  }
+}
