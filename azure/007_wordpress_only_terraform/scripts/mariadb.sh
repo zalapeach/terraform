6f1a546ce400de72e8fdae9115c70531e86815af
Juan Aguilar <Juan.Aguilar@epam.com>
2024-01-30 16:14:31 -0600

    comment script

--- azure/007_wordpress_only_terraform/scripts/mariadb.sh
+++ azure/007_wordpress_only_terraform/scripts/mariadb.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-runuser -l zala -c "sudo apt update -y"
-runuser -l zala -c "sudo apt install mariadb-server"
-runuser -l zala -c "mysql -u root <<-EOF
-  UPDATE mysql.user SET Password=PASSWORD('$pass') WHERE User='root'
-EOF"
+#runuser -l zala -c "sudo apt update -y"
+#runuser -l zala -c "sudo apt install mariadb-server"
+#runuser -l zala -c "mysql -u root <<-EOF
+  #UPDATE mysql.user SET Password=PASSWORD('$pass') WHERE User='root'
+  #DELETE FROM mysql.user
+#EOF"
