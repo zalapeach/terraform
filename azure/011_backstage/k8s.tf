bace24c4c68a6176c09dfd4ea5dd51a6052347ae
Juan Aguilar <Juan.Aguilar@epam.com>
2024-05-29 13:40:48 -0600

    creates a secret

--- azure/011_backstage/k8s.tf
+++ azure/011_backstage/k8s.tf
@@ -8,4 +8,14 @@ resource "kubernetes_namespace" "backstage" {
   }
 }
 
+resource "kubernetes_secret" "postgres" {
+  metadata {
+    name      = "postgres-secrets"
+    namespace = kubernetes_namespace.backstage.metadata[0].name
+  }
 
+  data {
+    POSTGRES_USER     = "TEST"
+    POSTGRES_PASSWORD = "TEST"
+  }
+}
