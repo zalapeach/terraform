566e16bdf6242164c9139fe66bbe2d79daa85e58
Juan Aguilar <Juan.Aguilar@epam.com>
2024-05-29 17:10:19 -0600

    increase azurerm version, fixes in postgres flexible server

--- azure/011_backstage/postgres.tf
+++ azure/011_backstage/postgres.tf
@@ -6,9 +6,9 @@ resource "azurerm_postgresql_flexible_server" "server" {
   name                   = "backstage-server-demo"
   resource_group_name    = azurerm_resource_group.rg.name
   location               = azurerm_resource_group.rg.location
-  version                = 15
+  version                = 16
   administrator_login    = "zala"
   administrator_password = random_password.pass.result
   storage_mb             = 32768
-  sku_name               = "GP_Standard_D2ds_v4"
+  sku_name               = "B_Standard_B1ms"
 }
