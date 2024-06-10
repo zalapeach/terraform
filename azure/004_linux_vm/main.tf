2dce0157480d4090341bf1576409d134323aada0
Juan Aguilar <Juan.Aguilar@epam.com>
2024-06-10 08:42:30 -0600

    increase version, change address in vnet

--- azure/004_linux_vm/main.tf
+++ azure/004_linux_vm/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.61.0"
+      version = "~>3.107.0"
     }
   }
   cloud {
@@ -24,7 +24,7 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet01"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.1.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
@@ -33,7 +33,7 @@ resource "azurerm_subnet" "subnet" {
   name                 = "subnet01"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.0.0/24"]
+  address_prefixes     = ["10.1.0.0/24"]
 }
 
 resource "azurerm_public_ip" "pip" {
