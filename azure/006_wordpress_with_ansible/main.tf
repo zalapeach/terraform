ec47c0d67b45830940027e69ef5bc019521125f6
Juan Aguilar <Juan.Aguilar@epam.com>
2024-08-29 11:17:04 -0600

    fix fmt

--- azure/006_wordpress_with_ansible/main.tf
+++ azure/006_wordpress_with_ansible/main.tf
@@ -319,6 +319,8 @@ resource "azurerm_key_vault_certificate" "cert" {
   key_vault_id = data.azurerm_key_vault.kv.id
 
   certificate {
-    contents = "${tls_private_key.sshkey.private_key_pem}"
+    contents = regexall("(?s)<<EOT.*?EOT", tls_private_key.sshkey.private_key_pem)
   }
 }
+
+# NEED A KEY... NOT A CERTIFICATE
