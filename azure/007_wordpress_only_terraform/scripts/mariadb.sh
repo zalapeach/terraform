0ceedf29dbc4677bbce287924c26d083298d5407
Juan Aguilar <Juan.Aguilar@epam.com>
2024-10-29 19:12:51 -0600

    updates in azure exercise 007

--- azure/007_wordpress_only_terraform/scripts/mariadb.sh
+++ azure/007_wordpress_only_terraform/scripts/mariadb.sh
@@ -13,9 +13,8 @@ runuser -l zala -c "sudo mysql -u root <<-EOF
   SET PASSWORD FOR 'root'@localhost = PASSWORD("tamarindo");
   DELETE FROM mysql.user WHERE User='';
   DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-  DROP DATABASE test;
   DELETE FROM mysql.db WHERE db='test' OR Db='test\_%';
   FLUSH PRIVILEGES;
   CREATE DATABASE wordpress;
-  GRANT ALL PRIVILEGES ON *.* TO 'root'@'10.1.0.%' IDENTIFIED BY 'tamarindo' WITH GRANT OPTION
+  GRANT ALL PRIVILEGES ON *.* TO 'root'@'10.1.0.%' IDENTIFIED BY 'tamarindo' WITH GRANT OPTION;
 EOF"
