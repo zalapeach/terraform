3e21a426cc257d087c1dd33d6edf713b0de4ac89
Juan Aguilar <Juan.Aguilar@epam.com>
2025-05-06 12:44:32 -0600

    bump versions

--- azure_devops/providers.tf
+++ azure_devops/providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azuredevops = {
       source  = "microsoft/azuredevops"
-      version = "~> 1.8.1"
+      version = "~> 1.9.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.24.0"
+      version = "~> 4.26.0"
     }
   }
   cloud {
