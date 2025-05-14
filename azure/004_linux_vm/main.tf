5018f6d23dcf4991a52b47bb943806047fbc1b8e
Juan Aguilar <Juan.Aguilar@epam.com>
2025-05-14 12:11:35 -0600

    changed allocation method

--- azure/004_linux_vm/main.tf
+++ azure/004_linux_vm/main.tf
@@ -40,7 +40,7 @@ resource "azurerm_public_ip" "pip" {
   name                = "publicIp"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
 resource "azurerm_network_security_group" "nsg" {
