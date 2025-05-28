d8626b176350a3cbac6cba81075f5bcb71c6204d
Juan Aguilar <Juan.Aguilar@epam.com>
2025-05-28 09:08:12 -0600

    grant permissions to manage identity

--- azure/013_managed_identities/main.tf
+++ azure/013_managed_identities/main.tf
@@ -16,3 +16,37 @@ data "azuredevops_project" "terraform" {
 resource "azuredevops_service_principal_entitlement" "identity" {
   origin_id = azurerm_user_assigned_identity.identity.principal_id
 }
+
+resource "azuredevops_group" "atlas_automation" {
+  scope        = data.azuredevops_project.terraform.id
+  display_name = "Terraform Automation"
+  description  = "Terraform Automation group to access repos and pipelines"
+  members = [
+    azuredevops_service_principal_entitlement.identity.descriptor
+  ]
+}
+
+resource "azuredevops_git_permissions" "atlas_access" {
+  project_id = data.azuredevops_project.atlas.id
+  principal  = azuredevops_group.atlas_automation.id
+  permissions = {
+    CreateRepository      = "Deny"
+    DeleteRepository      = "Deny"
+    RenameRepository      = "Deny"
+    GenericRead           = "Allow"
+    GenericContribute     = "Allow"
+    PullRequestContribute = "Allow"
+  }
+}
+
+resource "azuredevops_build_folder_permissions" "atlas_access" {
+  project_id = data.azuredevops_project.atlas.id
+  path       = "\\"
+  principal  = azuredevops_group.atlas_automation.id
+  permissions = {
+    "ViewBuilds" : "Allow",
+    "QueueBuilds" : "Allow",
+    "ViewBuildDefinition" : "Allow",
+    "QueueBuilds" : "Allow"
+  }
+}
